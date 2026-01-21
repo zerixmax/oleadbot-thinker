1d9dd7024c8cba3caf4f3a3a3bad183a7d87e5e9
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-01-21 19:03:57 +0100

    Update tkinter_01.py

--- tkinter_01.py
+++ tkinter_01.py
@@ -1,14 +1,29 @@
 import tkinter as tk
 
 
+def btn_clicked():
+    print('Gumb je kliknut.')
+
+
 root = tk.Tk()
 root.title('TkInter DEMO')
 root.geometry('600x400')
 
-lbl_intro_message = tk.Label(root,
-                             text='Pozdrav iz TkIntera!',
-                             font=('Verdana', 18))
-lbl_intro_message.pack(padx=10, pady=10)
+lbl_title = tk.Label(root,
+                     text='Pozdrav iz TkIntera!',
+                     font=('Verdana', 18))
+lbl_title.pack(padx=10, pady=10)
+lbl_subtitle = tk.Label(root,
+                        text='Ovo je moja prva TkInter aplikacija',
+                        font=('Verdana', 14))
+lbl_subtitle.pack(padx=10, pady=10)
+
+
+btn_click = tk.Button(root,
+                      text='Klikni me!',
+                      font=('Verdana', 14),
+                      command=btn_clicked)
+btn_click.pack(padx=10, pady=10)
 
 
 root.mainloop()
